351f4126de7414537a3a23d4c94a7f91e50d1deb
kitajima-kaho <kaho-kitajima@nakajitsu.com>
2022-10-17 18:06:44 +0900

    ヘッダー完了、メイン作業中

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,3 +1,56 @@
+<script>
+export default {
+    data() {
+        return {
+            status: 'clear',
+            attentionBlue: false,
+            attentionRed: false,
+            attentionGreen: false,
+            attentionYellow: false,
+
+        }
+    },
+
+    methods: {
+        start() {
+            this.status = 'start';
+            // this.attentionRed = true;
+
+            setTimeout(() => {
+                this.attentionBlue = true}
+                ,100
+            ).then(setTimeout(() => {
+                this.attentionRed = true}
+                ,200
+            ))
+
+            // setTimeoutAsync(0) 
+            // .then(() => {
+            //     this.attentionBlue = true;
+            //     return setTimeoutAsync(100);
+            // })
+            // .then(() => {
+            //     this.attentionRed = true;
+            //     return setTimeoutAsync(100);
+            // })
+            // .then(() => {
+            //     this.attentionGreen = true;
+            //     return setTimeoutAsync(100);
+            // })
+            // .then(() => {
+            //     this.attentionYellow = true;
+            // })
+        },
+
+        
+
+        stop() {
+            this.status = "stop"
+        }
+    }
+}
+</script>
+
 <template>
 <div id="page">
     <header>
@@ -16,41 +69,44 @@
         </div>
     </header>
     <main>
-        <article class="box media">
+        <div class="main_wrap">
+            <article class="box media">
 
-            <!-- <div class="roulette"></div> -->
             <div class="roulette_cover">
-                <div class="target orange"></div>
-                <div class="target yellow"></div>
-                <div class="target orange"></div>
-                <div class="target yellow"></div>
-                <div class="target orange"></div>
-                <div class="target yellow"></div>
+                <div class="target" :class="{color_blue : attentionBlue}"></div>
+                <div class="target" :class="{color_red : attentionRed}"></div>
+                <div class="target" :class="{color_green : attentionGreen}"></div>
+                <div class="target" :class="{color_yellow : attentionYellow}"></div>
             </div>
 
 
             <button class="button is-warning is-rounded is-large is-responsive" v-if="status !== 'start'" @click="start()">スタート</button>
             <button class="button is-warning is-rounded is-large is-responsive" v-else @click="stop()">ストップ</button>
 
+            </article>
+            <aside class="box side">
+            </aside>
 
-        </article>
-        <aside class="box side">
-        </aside>
-
+        </div>
     </main>
 </div>
         
 </template>
+
 <style lang="scss">
 * {
     margin:0;
     padding:0;
 }
 
+.page {
+    max-width: 1425px;
+}
+
 header {
     .header_container {
         background: url(~/assets/image/foodiesfeed.com_traditional-korean-meals.jpg) no-repeat center / cover ;
-        height: 200px;
+        height: 150px;
         background-position: 0 70%;
         display: flex;
         background-color: rgba(#FF8A02, 0.3);
@@ -62,7 +118,7 @@ header {
     }
 
     .search_field {
-        margin-left: 300px;
+        margin-left: 180px;
         width: 500px;
         display: flex;
 
@@ -88,52 +144,116 @@ header {
 }
 
 main {
-    max-width: 100%;
-    min-height:100vh;
+    width: 100%;
     background-color: #FCF4EC;
-    display: flex;
 
-    .media {
+
+    .main_wrap {
+        margin-right: auto;
+        margin-left: auto;
+        max-width: 925px;
+        min-height:100vh;
+        display: flex;
+        // background-color: blue;
+
+        .media {
         margin-top: 20px;
-        margin-left: 345px;
-        width: 865px;
-        height: 680px;
+        width: 620px;
+        height: 580px;
         display: block;
 
-        .roulette {
-            background-color: #FF8A02;
-            width: 500px;
-            height: 500px;
-            border-radius: 50%;
-            margin: 10px auto;
-        }
+            .roulette_cover {
+                background-color: #FF8A02;
+                width: 470px;
+                height: 470px;
+                border-radius: 50%;
+                margin: 5px auto;
+                position: relative;
 
-        .roulette {
-            background-color: #FF8A02;
-            width: 500px;
-            height: 500px;
-            border-radius: 50%;
-            margin: 10px auto;
-        }
+                .target {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    width: 235px;
+                    height: 235px;
 
-        .button {
-            display: block;
-            margin: 50px auto;
 
-        }
+                    .color_red {
+                    }
+
+                    &:first-child {
+                        position: absolute;
+                        top: 0px;
+                        right: 0px;
+                        border-radius: 0 100% 0 0;
+                        background-color: blue;
+
+                        &:not(.color_blue) {
+                            background-color: #bbdbf3;
+                        }
+                    }
+
+                    &:nth-child(2) {
+                        position: absolute;
+                        bottom: 0px;
+                        right: 0px;
+                        border-radius: 0 0 100% 0;
+                        background-color: red;
+
+                        &:not(.color_red) {
+                            background-color: #e3acae;
+                        }
+                    }
+
+
+
+                  
+
+                    &:nth-child(3) {
+                        position: absolute;
+                        bottom: 0px;
+                        left: 0px;
+                        border-radius: 0 0 0 100%;
+                        background-color: green;
+
+                        &:not(.color_green) {
+                            background-color: #a3d6ce;
+                        }
+                    }
+
+                    &:last-child {
+                        position: absolute;
+                        top: 0px;
+                        left: 0px;
+                        background-color: yellow;
+                        border-radius: 100% 0 0 0;
+
+                        &:not(.color_yellow) {
+                            background-color: #ffedab;
+
+                        }
+                    }
+
+                }
+            }
+
+            .button {
+                display: block;
+                margin: 10px auto;
+
+            }
+
+
+
+        }  
+
+        .side {
+            margin: 20px 0px auto 20px;
+            width: 300px;
+            height: 580px;
+            }
+ 
     }
-
-    .side {
-        margin: 20px 345px auto 30px;
-        width: 320px;
-        height: 680px;
-    }
-
-    .nox {
-        display: block;
-    }
-
-
 }
 
 
@@ -148,24 +268,4 @@ main {
     width: 30%;
   }
 }
-
 </style>
-<script>
-export default {
-    data() {
-        return {
-            status: 'clear',
-        }
-    },
-
-    methods: {
-        start() {
-           this.status = 'start';
-        },
-
-        stop() {
-            this.status = "stop"
-        }
-    }
-}
-</script>

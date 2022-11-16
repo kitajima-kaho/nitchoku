766147e33f3ecf7a4bd7c328785599c70888b9c5
kitajima-kaho <kaho-kitajima@nakajitsu.com>
2022-11-16 11:15:52 +0900

    ルーレット赤修正。

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -732,7 +732,7 @@ export default {
 
                                     img {
                                         filter: grayscale(80%);
-                                        opacity: 0.9; 
+                                        opacity: 0.7; 
                                     }                      
                                 }   
                             }
